1c2b9ae1da827a0ddc7ef79e1db1a44b66ee6722
yubie <ayubali2443@gmail.com>
2025-12-22 14:53:53 -0800

    Add calculator operation structure & operate func
    - Defined variables to store the first number,
    operator, and second number of a calculator operation
    - Added an operate function that accepts an operator
    and two numbers and delegates execution to the
    appropriate calculation function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+let operand1;
+let operand2;
+let operator;
+
 function add (num1, num2) {
     return num1 + num2;
 }
@@ -14,4 +18,23 @@ function multiply (num1, num2) {
 
 function divide (num1, num2) {
     return num1 / num2;
+}
+
+function operate (operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return add (num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+        default:
+            break;
+    }
 }
